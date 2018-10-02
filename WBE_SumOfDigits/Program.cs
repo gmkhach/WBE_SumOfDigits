c2253302ffdaa566ef570b17c3dc7fe259350b21
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-02 15:41:42 -0400

    brute-force method implemented

--- WBE_SumOfDigits/Program.cs
+++ WBE_SumOfDigits/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*
+ * Write a method that takes a number and returns the sum of all of its integers.
+ */ 
+ 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +14,43 @@ namespace WBE_SumOfDigits
     {
         static void Main(string[] args)
         {
+            do
+            {
+                try
+                {
+                    Console.Write("\nEnter a positive integer\n\n>>> ");
+                    int number = int.Parse(Console.ReadLine().Trim());
+                    if (number > 0 && number % 1 == 0)
+                    {
+                        Console.WriteLine($"\nSum of digits: {SumOfDigits(number)}");
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        throw new Exception("Invalid Entry!");
+                    }
+                    Console.Write("\nPress Enter to try a different number...");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                catch (Exception ex)
+                {
+                    Console.Clear();
+                    Console.WriteLine(ex.Message);
+                }
+
+            } while (true);
+        }
+
+        static int SumOfDigits(int umber)
+        {
+            int sum = 0;
+            do
+            {
+                sum += umber % 10;
+                umber /= 10;
+            } while (umber !=0);
+            return sum;
         }
     }
 }
